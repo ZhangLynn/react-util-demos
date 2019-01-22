7f9256241fc2c3b0fdc813cc6d389f458a6a9805
ZhangLynn <zl8312070@sina.com>
2019-01-22 23:42:44 +0800

    feat: 新增form的reducer

--- src/reducers.js
+++ src/reducers.js
@@ -2,8 +2,9 @@
 import { combineReducers } from 'redux';
 import {reducer as todoReducer} from './components/todos';
 import {reducer as filterReducer} from './components/filter';
-
+import formReducer from './components/reduxForm/reducer'
 export default combineReducers({
     todos: todoReducer,
-    filter: filterReducer
+    filter: filterReducer,
+    form: formReducer
 });
