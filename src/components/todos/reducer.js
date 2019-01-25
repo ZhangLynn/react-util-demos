71ef7ccfff8ca56a7e018f8c6c7b9d93edeef188
ZhangLynn <zl8312070@sina.com>
2019-01-25 20:05:49 +0800

    refactor: 重构todolist组件

--- src/components/todos/reducer.js
+++ src/components/todos/reducer.js
@@ -1,33 +1,50 @@
-import {ADD_TODO, TOGGLE_TODO, REMOVE_TODO}from './actionTypes.js';
+import {ADD_TODO, TOGGLE_TODO, REMOVE_TODO} from './actionTypes.js'
+
+function updateTodo(state, newValue) {
+    return [
+        newValue,
+        ...state
+    ]
+}
 
 export default (state = [], action) => {
-  switch(action.type) {
-    case ADD_TODO: {
-      return [
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        },
-        ...state
-      ]
-    }
-    case TOGGLE_TODO: {
-      return state.map((todoItem) => {
-        if (todoItem.id === action.id) {
-           return {...todoItem, completed: !todoItem.completed};
-        } else {
-          return todoItem;
+    switch (action.type) {
+        case ADD_TODO: {
+            alert(1)
+            const list = [
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                },
+                ...state
+            ];
+            console.log(list)
+            // return [
+            //     {
+            //         id: action.id,
+            //         text: action.text,
+            //         completed: false
+            //     },
+            //     ...state
+            // ]
         }
-      })
+        case TOGGLE_TODO: {
+            return state.map((todoItem) => {
+                if (todoItem.id === action.id) {
+                    return {...todoItem, completed: !todoItem.completed}
+                } else {
+                    return todoItem
+                }
+            })
+        }
+        case REMOVE_TODO: {
+            return state.filter((todoItem) => {
+                return todoItem.id !== action.id
+            })
+        }
+        default: {
+            return state
+        }
     }
-    case REMOVE_TODO: {
-      return state.filter((todoItem) => {
-        return todoItem.id !== action.id;
-      })
-    }
-    default: {
-      return state;
-    }
-  }
 }
--- src/pages/todoapp.js
+++ src/pages/todoapp.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {view as Todos} from '../components/todos/index';
 import {view as Filter} from '../components/filter/index';
-
+import {view as AsyncHttp} from '../components/asyncHttp/index'
 function TodoApp(props) {
     return (
         <div>
-            <div></div>
             <Todos/>
             <Filter/>
+            <AsyncHttp/>
         </div>
     );
 }
